483b63430b59763193ef8d4e720522d01c9ce1d5
2K39 <ahmed.yousif.k@gmail.com>
2021-09-12 23:09:33 +0300

    add

--- converter.py
+++ converter.py
@@ -1,10 +1,7 @@
-import csv
-import json
-import pandas as pd
-import os
+import csv , os , json , pandas as pd
+
 csv_file = 'data.csv'
 json_file = 'data.json'
-
 
 csv_file = pd.DataFrame(pd.read_csv(csv_file, sep = ",", header = 0, index_col = False))
 csv_file.to_json(json_file, orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
